9a4ead2970deff6a8fc7314b2088d760a5dabcf7
fanuelal <fanuelalmawstop@gmail.com>
2022-04-05 10:12:59 -0700

    lint fixed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-      task = task2;  
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
   }
 
   return [task, task2];
