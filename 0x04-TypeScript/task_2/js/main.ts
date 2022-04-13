7f9205bc49c2a8259e9c0a0a76e2349669724b51
fanuelal <fanuelalmawstop@gmail.com>
2022-04-13 13:26:17 -0700

    task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -25,3 +25,12 @@ export const createEmployee = (salary: number | string): Director | Teacher => {
     if (typeof salary === 'number' && salary < 500) return new Teacher();
     else return new Director();
 }
+
+export const isDirector = (employee: Director | Teacher): employe is Director => {
+  return employee instanceof Director;
+}
+
+export const executeWork = (employee: Director | Teacher): string => {
+  if (isDirector(employee)) return employee.workDirectorTasks();
+  else return employee.workTeacherTasks();
+}
