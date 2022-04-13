61a5d3e8f5ef8fc18aa097ea8ba959f0a97f691f
fanuelal <fanuelalmawstop@gmail.com>
2022-04-13 13:31:57 -0700

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,3 +34,10 @@ export const executeWork = (employee: Director | Teacher): string => {
   if (isDirector(employee)) return employee.workDirectorTasks();
   else return employee.workTeacherTasks();
 }
+
+export type Subjects = 'Math' | 'History';
+
+export const teachClass = (todayClass: Subjects): string => {
+  if (todayClass == 'Math') return 'Teaching Math';
+  else if (todayClass == 'History') return 'Teaching History'; 
+}
