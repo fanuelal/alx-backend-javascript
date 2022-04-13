0d8a7f5c2f0355abba314a2241b1fb37571a37d6
fanuelal <fanuelalmawstop@gmail.com>
2022-04-13 13:19:33 -0700

    task 5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,27 @@
+export interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+export interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+export class Director implements DirectorInterface {
+    workFromHome() { return 'Working from home'; }
+    getCoffeeBreak() { return 'Getting a coffee break'; }
+    workDirectorTasks() { return 'Getting to director tasks'; }
+}
+
+export class Teacher implements TeacherInterface {
+    workFromHome() { return 'Cannot work from home'; }
+    getCoffeeBreak() { return 'Cannot have a break';}
+    workTeacherTasks() {return 'Getting to work';}
+}
+
+export const createEmployee = (salary: number | string): Director | Teacher => {
+    if (typeof salary === 'number' && salary < 500) return new Teacher();
+    else return new Director();
+}
